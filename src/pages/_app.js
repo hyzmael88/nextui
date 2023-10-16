3a0c90710f4b0a178e4acb9612933aaf3e75ff32
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2023-10-15 22:43:31 -0600

    inicio

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
 
+// pages/_app.js
+import {NextUIProvider} from '@nextui-org/react'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
